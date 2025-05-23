004c9aa30c84fb2ae143907d6e5a2006dd53b4cd
zomba40k <astapov-n@bk.ru>
2025-05-23 15:02:09 +0300

    added PageObjects

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,8 @@
-import time
-import pytest
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
-
-def test_is_button_present(browser):
-    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/.")
-    time.sleep(2)
-    button = browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form > button')
-    assert button.is_displayed(), 'Кнопка не найдена'
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()
