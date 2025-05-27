68ddfdf41e6fa4c5e3a1a644bb4432ed2671230b
zomba40k <astapov-n@bk.ru>
2025-05-27 10:22:15 +0300

    added login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .locators import LoginPageLocators
+from .base_page import BasePage
+import selenium
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in browser.current_url,'incorrect URL'
+
+    def should_be_login_form(self):
+
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Login form not found'
+
+    def should_be_register_form(self):
+
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Register form not found'
